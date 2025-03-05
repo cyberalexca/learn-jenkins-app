e4a8027aada28666fd39857127011eb93d512a24
Olexiy Braga <cyberalex@gmail.com>
2025-03-05 20:50:46 +0000

    add new line

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         </a>
       </header>
       <p>
-          Application version: 1
+          Application version: 1 <br />
           Commit version: {process.env.REACT_APP_GIT_HASH || "unknown"}
       </p>
     </div>
